6c6b5e0fe9e509d7944673124c839352068afcc4
Aiver7 <aiverson.palacios@udea.edu.co>
2025-10-28 17:44:57 -0500

    Se implentaron las funciones escribir en un archivo y la que pasa un texto a binario

--- Lab3_Ejercicios/tools.cpp
+++ Lab3_Ejercicios/tools.cpp
@@ -19,3 +19,32 @@ string read(string name)
 
     return data;
 }
+void write(string name, string data)
+{
+    ofstream file;
+
+    file.open(name);
+    if (file.is_open()){
+        file << data;
+        file.close();
+    }
+    else{
+        cout << "Error al abrir el archivo." << endl;
+        exit(1);
+    }
+}
+string text_to_binary(string text)
+{
+    string binary, char_to_bin;
+
+    while(text.length()){
+        char_to_bin = "";
+        for(int i=0, ascii=int(text[0]); i<8; ascii /= 2, ++i){
+            char_to_bin += (ascii%2) + 48;
+        }
+        reverse(char_to_bin.begin(), char_to_bin.end());
+        binary += char_to_bin;
+        text = text.substr(1, text.length()-1);
+    }
+
+    return binary;
--- Lab3_Ejercicios/tools.h
+++ Lab3_Ejercicios/tools.h
@@ -2,9 +2,13 @@
 #define TOOLS_H
 
 #include <iostream>
+#include <algorithm>
 #include <fstream>
 #include <string>
 
-using namespace std;
+string read(string name);
+void write(string name, string data);
+string first_method(string data,int seed);
+string text_to_binary(string text);
 
 #endif // TOOLS_H
